e6d5aa55a8a3d4f28331ccf5dd12293190d47c2f
myto1 <myto1@o2.pl>
2022-11-24 21:56:19 +0100

    1

--- app/src/main/java/com/empro/kalk_u_lator/DialogWindow2.java
+++ app/src/main/java/com/empro/kalk_u_lator/DialogWindow2.java
@@ -25,7 +25,7 @@ public class DialogWindow2 extends AppCompatDialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.d_change_dialog, null);
         builder.setView(view)
-                .setTitle("Podaj nową grubość warstwy")
+                .setTitle("Podaj nową grubość warstwy:")
                 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
